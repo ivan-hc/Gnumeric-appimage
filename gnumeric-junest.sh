d304599c894e32f4f00a2b271de26da1e7169b54
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-01-22 00:32:13 +0100

    Update gnumeric-junest.sh

--- gnumeric-junest.sh
+++ gnumeric-junest.sh
@@ -7,9 +7,6 @@ DEPENDENCES=""
 #BASICSTUFF="binutils gzip"
 #COMPILERS="base-devel"
 
-# ADD A VERSION, THIS IS NEEDED FOR THE NAME OF THE FINEL APPIMAGE, IF NOT AVAILABLE ON THE REPO, THE VALUE COME FROM AUR, AND VICE VERSA
-VERSION=$(wget -q https://archlinux.org/packages/extra/x86_64/$APP/ -O - | grep $APP | head -1 | grep -o -P '(?<='$APP' ).*(?=</)' | tr -d " (x86_64)")
-
 # CREATE THE APPDIR (DON'T TOUCH THIS)...
 wget -q https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 chmod a+x appimagetool
@@ -117,7 +114,7 @@ cd ..
 # EXTRACT PACKAGE CONTENT
 mkdir base
 tar fx $(find ./$APP.AppDir -name $APP-[0-9]*zst | head -1) -C ./base/
-
+VERSION=$(cat ./base/.PKGINFO | grep pkgver | cut -c 10-)
 mkdir deps
 
 ARGS=$(echo "$DEPENDENCES" | tr " " "\n")
